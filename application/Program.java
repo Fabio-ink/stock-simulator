55f95d21bd5394b70a89c5070fa5957e19e7b8c1
Fabio <fabiopradaa@gmail.com>
2025-07-02 20:50:36 -0300

    Adição do contrutor utilizando sobrecarga e aplicação de setters no Program

--- application/Program.java
+++ application/Program.java
@@ -21,16 +21,44 @@ public class Program {
         System.out.println();
         System.out.println("Informações do produto: " + product);
         System.out.println();
-        System.out.println("Digite o número de unidades para entrar no estoque:");
+        System.out.print("Digite o número de unidades para entrar no estoque: ");
         quantity = sc.nextInt();
         product.addProduct(quantity);
 
         System.out.println();
         System.out.println("Informações atualizas para: " + product);
         System.out.println();
-        System.out.println("Digite o número de unidades para sair do estoque:");
+        System.out.print("Digite o número de unidades para sair do estoque: ");
         quantity = sc.nextInt();
         product.removeProduct(quantity);
+
+        System.out.println();
+        System.out.println("Informações atualizas para: " + product);
+
+        /*utilização dos métodos set para alterar as informações do produto.
+         * Caso eu quisesse utilizar o get seria: System.out.println("Informações atualizas para: " + product.getName());
+        */
+        
+        System.out.println();
+        System.out.println("Deseja alterar o produto? (s/n)");
+        char nameResponse = sc.next().charAt(0);;
+        if (nameResponse == 's'){
+            System.out.println();
+            System.out.print("Digite o nome do produto: ");
+            String newProduct = sc.next();
+            product.setName(newProduct);
+        } 
+
+        System.out.println();
+        System.out.println("Deseja alterar o preço do produto? (s/n)");
+        char priceResponse = sc.next().charAt(0);
+        if (priceResponse == 's'){
+            System.out.println();
+            System.out.print("Digite o preço do produto: ");
+            Double newPrice = sc.nextDouble();
+            product.setPrice(newPrice);
+        }
+
         System.out.println();
         System.out.println("Informações atualizas para: " + product);
 
--- entities/Product.java
+++ entities/Product.java
@@ -2,14 +2,39 @@ package entities;
 
 public class Product {
     
-    public String name;
-    public Double price;
-    public int quantity;
+    private String name;
+    private Double price;
+    private int quantity;
 
     public Product(String name, Double price, int quantity){
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public Product(String name, Double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Double getPrice(){
+        return price;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPrice(Double price){
+        this.price = price;
     }
 
     public Double totalValueStock(){
